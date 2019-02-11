f4cc86e9e6f0db6420032a4abe155d553e1169c5
Robert Kerr <rob@robertkerr.me>
2019-02-11 15:41:49 -0600

    added api key to ignored file and imported that file into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './Dashboard';
+import Config from './.config.js'
 
 class App extends Component {
   constructor(props) {
@@ -9,8 +10,7 @@ class App extends Component {
       tokens: ['BTC', 'ETH', 'LTC', 'XRP', 'BCH', 'NEO', 'DASH', 'ZEC', 'XMR'],
       searchTerms: null,
       priceData: [],
-      apkiKey: '228ef4fe3191079e9bf60b75b834bd284ac7d9e186df5a579269d04bf83dd5e7',
-      baseMultipriceURL : 'https://min-api.cryptocompare.com/data/pricemultifull?',
+      apkiKey: Config.apkiKey, //stored in file .config.js which is listed in .gitignore
     }
   }
 
@@ -27,13 +27,16 @@ class App extends Component {
     clearInterval(this.timerId);
   }
 
+  buildFetchURL() {
+    const tokens = this.state.searchTerms ? this.state.searchTerms : this.state.tokens;    
+    return 'https://min-api.cryptocompare.com/data/pricemultifull?'
+    + 'fsyms=' + tokens.join(',') //join token shortcodes into string
+    + '&tsyms=USD'
+    + '&api_key=' + this.state.apiKey;//store this in hidden file
+  }
+
   async refreshData() {
-    const tokens = this.state.searchTerms ? this.state.searchTerms : this.state.tokens;
-    const response = await fetch(
-      this.state.baseMultipriceURL
-      + 'fsyms=' + tokens.join(',') //join token shortcodes into string
-      + '&tsyms=USD'
-      + '&api_key=' + this.state.apiKey, //store this in hidden file
+    const response = await fetch(this.buildFetchURL(), 
       {
         method: 'GET',
         headers: {
@@ -59,7 +62,6 @@ class App extends Component {
   }
 
   render() {
-    // this.refreshData();
     return (
       <div className="App">
         <header className="App-header">
